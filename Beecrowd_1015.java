9f347981abc31fa0637fcc5ab7447770d0bd5e88
Vitorir <100983862+Vitorir@users.noreply.github.com>
2022-03-05 03:23:56 -0300

    Update Beecrowd_1015.java

--- Beecrowd_1015.java
+++ Beecrowd_1015.java
@@ -1,4 +1,4 @@
-// Distância Entre Dois Pontos
+// 1015 - Distância Entre Dois Pontos
 
 import java.io.IOException;
 import java.util.Scanner;
@@ -7,15 +7,19 @@ import java.lang.Math;
 public class Main {
  
     public static void main(String[] args) throws IOException {
-    Scanner s = new Scanner(System.in);
-		double x1, x2, y1, y2;
+    	Scanner s = new Scanner(System.in);
+		
+	    // Entrada
+	    double x1, x2, y1, y2;
 		x1 = s.nextDouble();
 		y1 = s.nextDouble();
 		x2 = s.nextDouble();
 		y2 = s.nextDouble();
-		
-    double distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    System.out.println(String.format("%.4f", distancia)); 
+	    
+	    // importar biblioteca Math (import java.lang.Math)
+    	    double distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)); 
+    		
+	    System.out.printf("%.4f\n", distancia); 
     }
  
 }
