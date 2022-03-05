5c7ec704e6149df32a405cd9b847934c18d9cce2
Vitorir <100983862+Vitorir@users.noreply.github.com>
2022-03-05 03:09:48 -0300

    Update Beecrowd_1005.java

--- Beecrowd_1005.java
+++ Beecrowd_1005.java
@@ -1,18 +1,23 @@
-import java.io.IOException;
-import java.util.Scanner; 
+// 1005 - Média 1
+
+import java.io.IOException; // Comando requerido no Beecrowd
+import java.util.Scanner;
 
 public class Main {
  
     public static void main(String[] args) throws IOException {
     	Scanner s = new Scanner(System.in);
 		
+    		// Entrada
 		double A = s.nextDouble();
 		double B = s.nextDouble();
 		
-		double soma = 3.5 * A + 7.5 * B;
-		double MEDIA = soma / 11;
+		// Processamento (Cálculo de Média Ponderada)
+		double soma = 3.5 * A + 7.5 * B; // Nota A vezes o peso de A + Nota B vezes peso da nota B
+		double MEDIA = soma / 11; // Divide o valor real pelo peso total 11
 
-		System.out.println(String.format("MEDIA = %.5f", MEDIA));
-    }
- 
+		// Saída
+		System.out.printf("MEDIA = %.5f\n", MEDIA);
+
+	}
 }
